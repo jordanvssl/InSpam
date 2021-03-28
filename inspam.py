b37eff131facdf5396af68313fe8cc27ae3fdc5c
Jordan Vssl <59113947+jordanvssl@users.noreply.github.com>
2021-03-28 22:32:36 +0200

    Update inspam.py

--- inspam.py
+++ inspam.py
@@ -5,8 +5,6 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from colored import fg, bg, attr
 
-
-
 #Var
 copy = "https://github.com/jordanvssl/InSpam "
 options = webdriver.ChromeOptions()
